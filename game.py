2bdf92c92b87d31381b44cd0a87ef86b4bba6c8e
AndrewCooley1 <ac8896@stern.nyu.edu>
2021-07-08 21:27:10 -0400

    Update game.py

--- game.py
+++ game.py
@@ -3,31 +3,34 @@
 import random
 
 
-
 print("Rock, Paper, Scissors, Shoot!")
 
 
 #Ask for user input
-#source python docs
+#source: https://docs.python.org/3/library/functions.html#input
 
-x = input("Please choose one of 'rock', 'paper', 'scissors'")
-print (x)
+x = input("Please choose one of 'rock', 'paper', 'scissors': ")
+print(x)
+
 #validate user input
 
 
-if (x =="rock") or (x == "paper") or (x == "scissors"):
-    print("valid")
+if (x == "rock") or (x == "paper") or (x == "scissors"):
+    print("VALID")
 else:
-    print("oops, invalid, please try again")
+    print("OOPS, INVALID, PLEASE TRY AGAIN")
     exit()
 
-print(user chose ("x")
+
+print("user chose: ",x)
 
 #generate computer choice
-#source from stackoverflow
+#https://docs.python.org/3/library/random.html
+#source: https://stackoverflow.com/questions/306400/how-to-randomly-select-an-item-from-a-list
 
 
 valid_options = ["rock", "paper", "scissors"] #list
 
-c = (random.choice(valid_options))
+c = random.choice(valid_options)
+print("COMPUTER Chose:", c)
 #determine the winner
