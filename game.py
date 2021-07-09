be9d31155b80301a76397ed94722c2150880b105
AndrewCooley1 <ac8896@stern.nyu.edu>
2021-07-08 20:36:24 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,3 +1,16 @@
 # game.py
 
 print("Rock, Paper, Scissors, Shoot!")
+
+
+#Ask for user input
+#source python docs
+
+x = input("Please choose one of 'rock', 'paper', 'scissors'")
+Print (x)
+#validate user input
+
+#generate computer choice
+
+
+#determine the winner
